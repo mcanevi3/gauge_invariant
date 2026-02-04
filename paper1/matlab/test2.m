7324b4352a6e84c5df887f8b6a4067e4bf5ef213
Mehmet Canevi <m.canevi3@gmail.com>
2026-02-04 22:03:18 +0300

    cont'd

--- paper1/matlab/test2.m
+++ paper1/matlab/test2.m
@@ -1,10 +1,9 @@
 clear;clc;
-%%% solvibg for L and solving for Lmbda
+%%% solving for L then solving for Lmbda
 A=-1;
 Bw=1;
 C=[1;2];
-Dw=[1;2];
-% L=[1,1];
+Dw=[1;2]*1+[2;-1]*1;
 n=size(A,1);
 p=size(C,1);
 
@@ -20,25 +19,13 @@ fprintf("Angle: %f\n",angle);
 
 Pival=eye(p)-C*pinv(C'*C)*C';
 
-% cvx_clear;
-% cvx_begin sdp quiet;
-% variable Lmbda(n,p)
-% minimize(norm(Bw+(-L-L*Pival+Lmbda*Pival)*Dw))
-% cvx_end;
-
-% if strcmp(cvx_status,'Solved')
-%     expr=Bw+(-L-L*Pival+Lmbda*Pival)*Dw;
-%     Lmbda
-%     expr
-% end
 Cz=1;
 cvx_clear;
 cvx_begin sdp quiet;
 variable P(n,n) symmetric;
 variable Y(n,p); % n,p
-variable Plambda(n,p); % n,p
 variable gmma;
-minimize(gmma+norm(P*Bw-Y*Dw-Y*Pival*Dw+Plambda*Pival*Dw));
+minimize(gmma);
 P>=1e-5*eye(n);
 [A'*P+P*A-Y*C-(Y*C)',P*Bw-Y*Dw,Cz';
 (P*Bw-Y*Dw)',-gmma*eye(1),zeros(1,1);
@@ -46,7 +33,37 @@ Cz,zeros(1,1),-gmma*eye(1)]<=0;
 cvx_end;
 
 L=inv(P)*Y;
-Lmbda=inv(P)*Plambda;
+disp("Eig:");
 disp(eig(A-L*C))
+disp("L:");
 disp(L)
-disp(Lmbda)
+
+%  L=[1,1];
+
+
+syms x1 x2 x3 x4 x5 x6 real;
+Lmbda=[x1,1];
+expr=Bw-L*Dw+L*Pival*Dw-Lmbda*Pival*Dw;
+x1val=double(solve(expr==0));
+disp("Prob:");
+disp(vpa(expr,4));
+Lmbda=[x1val,1];
+expr=Bw-L*Dw+L*Pival*Dw-Lmbda*Pival*Dw;
+disp(vpa(expr,4));
+
+% Lmbda=Lmbda*0;
+% Lmbda=[x1val,1]*0;
+% expr=Bw+(-L-L*Pival+Lmbda*Pival)*Dw
+
+% cvx_clear;
+% cvx_begin sdp quiet;
+% variable Lmbda(n,p)
+% minimize(norm(Bw-L*Dw+(L-Lmbda)*Dw))
+% cvx_end;
+
+% if strcmp(cvx_status,'Solved')
+%     expr=Bw-L*Dw+(L-Lmbda)*Dw;
+%     expr
+% end
+
+
