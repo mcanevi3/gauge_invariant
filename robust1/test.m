0b83c7f1febf36ed1dbd4515db18af791bba987d
Mehmet Canevi <m.canevi3@gmail.com>
2026-02-12 20:31:25 +0300

    cont'd

--- robust1/test.m
+++ robust1/test.m
@@ -5,22 +5,14 @@ Bw=1;
 Dw=[1;2]*1+[2;-1]*1;
 
 C0=[1;2];
-Ctheta=[2;3];
-
 H=diag(C0);
-syms d1 d2 real;
 E=1;
-H*[d1;d2]*E
 
 theta_bar=1;
-
-
-theta_test=-0.5;
-C=C0+(theta_test)*Ctheta;
  
 r=size(Bw,2);
 n=size(A,1);
-p=size(C,1);
+p=size(C0,1);
 
 proj=@(C)eye(p)-C*pinv(C'*C)*C';
 
@@ -35,7 +27,7 @@ variable gmma;
 minimize(gmma);
 P>=1e-5*eye(n);
 S >= 1e-5*eye(n);
-DeltaM = Y*Ctheta;
+DeltaM = Y*C0;
 [ A'*P+P*A - Y*C0 - (Y*C0)' + theta_bar^2*S, ...
   P*Bw - Y*Dw, Cz', DeltaM;
   (P*Bw - Y*Dw)', -gmma*eye(r), zeros(r,n), zeros(r,n);
@@ -45,18 +37,8 @@ cvx_end;
 
 L=inv(P)*Y;
 L
-L=[10,10];
-eig(A-L*C)
+L=[20,20];
+eig(A-L*C0)
 
 Pi0=proj(C0);
-Piub=proj(C0+theta_bar*Ctheta);
-Pitheta=proj(Ctheta);
 
-syms alpha real;
-C=C0+(alpha)*Ctheta;
-
-xt=0.01;
-yt=C*xt;
-
-temp=Pi0*yt;
-pinv(Pi0*Ctheta)*temp
