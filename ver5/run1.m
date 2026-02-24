b94a04ec26a93a47e6f77a30210d832920edd8c9
Mehmet Canevi <m.canevi3@gmail.com>
2026-02-24 22:24:33 +0300

    cont'd

--- ver5/run1.m
+++ ver5/run1.m
@@ -1,14 +1,15 @@
 clear;clc;
 
 syms x real;
-x=1;
-Cx=[cos(x);-sin(x)];
+%x=1;
+Cx=[cos(x);sin(x)];
 
 proj=@(C)eye(size(C,1))-C*pinv(C'*C)*C';
 Pival=proj(Cx);
-%Pival=simplify(Pival)
+Pival=simplify(Pival)
 
 expr=Pival*Cx;
 %simplify(expr)
 
+simplify(pinv(Pival*[1;2]))
 
