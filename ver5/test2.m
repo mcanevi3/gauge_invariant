4612734dac692e61c4a5ce9139a3e0b581978bf1
Mehmet Canevi <m.canevi3@gmail.com>
2026-02-25 22:15:26 +0300

    cont'd

--- ver5/test2.m
+++ ver5/test2.m
@@ -1,4 +1,5 @@
 clear;clc;
+%%% LO model1
 
 A=-1;
 Bw=1;
@@ -8,3 +9,11 @@ D=[3;3];
 
 proj=@(C)eye(size(C,1))-C*pinv(C'*C)*C';
 proj(C)
+
+lambda=-4;
+l2=-lambda-4/3;
+l1=1/3-l2;
+L=[l1,l2];
+eig(A-L*C)
+Bw-L*D
+
