78ff922a8a04637aedfb0d9e3155f489498ff36a
Mehmet Canevi <m.canevi3@gmail.com>
2026-02-26 12:56:40 +0300

    cont'c

--- ver5/test2.m
+++ ver5/test2.m
@@ -1,19 +1,24 @@
 clear;clc;
 %%% LO model1
-
 A=-1;
 Bw=1;
-
 C=[1;2];
 D=[3;3];
 
 proj=@(C)eye(size(C,1))-C*pinv(C'*C)*C';
-Pival=proj(C)
+Pival=proj(C);
 
 lambda=-4;
 l2=-lambda-4/3;
 l1=1/3-l2;
 L=[l1,l2];
-eig(A-L*C)
-Bw-L*D
+eig(A-L*C);
+Bw-L*D;
+
+syms w x real;
+y=C*x+D*w;
+expr=Pival*(y);
+y
+expr
+
 
