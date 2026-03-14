be3c6f4ccda4015bb63b4f6333664fb4838b4769
Mehmet Canevi <m.canevi3@gmail.com>
2026-03-14 23:08:03 +0300

    cont'd

--- paper2/matlab/run1.m
+++ paper2/matlab/run1.m
@@ -1,44 +1,30 @@
 clear;clc;
 
+%% System model
 A=[1,2;-3,-4];
-Bw=[2;1];
-C=[1,0;1,0;0,1];
-Dw=[1;2;3];
+Bw=[1;2];
+C=[1,0;2,0;1,2];
 
-L=sym('L',[2,3]);
+Cperp=null(C');
+Dw=Cperp+C*[1;1];
 
-temp=Bw-L*Dw;
-sol=solve(temp,L(:,1));
-Lval=subs(L,sol);
+n=size(A,1);
+p=size(C,1);
+%% Projection
+proj=@(C)eye(size(C,1))-C*pinv(C'*C)*C';
+Pival=proj(C);
 
-syms s;
-temp=det(s*eye(2)-(A-Lval*C));
-prob=coeffs(temp,s,'all')==conv([1 4],[1 5]);
-sol=solve(prob,L(:,2));
+%% Disturbance model
+Aw=0;
+Cw=1;
+nw=size(Aw,1);
+pw=size(Cw,1);
 
-Lval2=subs(Lval,sol);
-funL=matlabFunction(Lval2);
+%% Augmented model
+Aa=[A,Bw*Cw;zeros(nw,n),Aw];
+Ca=[C,Dw*Cw];
 
-Lval3=funL(1,1);
+La=place(Aa',Ca',[-10,-10,-10])'
 
-disp("L:");
-disp(Lval3);
-disp("LC:");
-disp(Lval3*C);
-disp("||LC||:"+string(norm(Lval3*C,inf)));
-% disp("Eig:");
-% disp(eig(A-Lval3*C));
-% disp("Bw-L*Dw");
-% disp(Bw-Lval3*Dw);
 
-Lval3=funL(10,12);
 
-disp("L:");
-disp(Lval3);
-disp("LC:");
-disp(Lval3*C);
-disp("||LC||:"+string(norm(Lval3*C,inf)));
-% disp("Eig:");
-% disp(eig(A-Lval3*C));
-% disp("Bw-L*Dw");
-% disp(Bw-Lval3*Dw);
