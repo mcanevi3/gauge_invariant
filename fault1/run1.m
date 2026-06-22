407f097180db756960913a81594c9493fbb0029e
Mehmet Canevi <m.canevi3@gmail.com>
2026-06-22 08:24:51 +0300

    fix

---
+++ fault1/run1.m
@@ -0,0 +1,30 @@
+clear;clc;
+
+proj=@(C)eye(size(C,1))-C*pinv(C'*C)*C';
+C=[1,0;
+   3,1;
+   2,1;
+   ];
+
+Pival=proj(C);
+
+syms f1 f2 f3 f4 f5 f6 real;
+syms x1 x2 real;
+x=[x1;x2];
+
+%y=C*x+eye(6)*[0;0;0;1;0;0];
+%vpa(Pival*y,5)
+
+y=C*x+[f1;f2;f3];
+vpa(Pival*y,5)
+
+y=C*x+[f1;0;0];
+vpa(Pival*y,5)
+
+y=C*x+[0;f2;0];
+vpa(Pival*y,5)
+
+y=C*x+[0;0;f3];
+vpa(Pival*y,5)
+
+% proj(kron(randn(1,2),ones(5,1)))*[f1,0;0,f2;0,0;0,0;0,0]
